b206d88488295693ffc814892af4b5201320e26a
Gustavo Gorgônio <ggorgon03@gmail.com>
2024-04-15 14:40:28 -0300

    add prediction algorithm

--- my_knn.py
+++ my_knn.py
@@ -8,32 +8,45 @@ class KNN:
     def fit(self, X_train, y_train):
         self.X_train = X_train
         self.y_train = y_train
-
-    def predict(self, X_test):
-        predictions = [self._predict(x) for x in X_test]
-        return np.array(predictions)
-
-    def _predict(self, x):
-        pass
     
     def _euclidean_distance(slef, xs: list[int], ys : list[int]) -> float:
-        if len(xs) > 1 and len(xs) == len(ys):
+        if len(xs) == 0 or len(ys) == 0 or len(xs) != len(ys):
             return -1
         
         acc = 0
-        for i in range(xs):
+        for i in range(len(xs)):
             acc += (xs[i] - ys[i])**2
         
         return np.sqrt(acc)
+    
+    def get_neighbord_distance(self,sample):
+        # get all distances from input sample
+        distances = []
+        for i in range(len(self.X_train)):
+            distances.append((self.y_train[i],self._euclidean_distance(self.X_train[i],sample)))
+            distances.sort(key=lambda x : x[1])
+        
+        neighbors=[]
+        for i in range(self.k): # get first k samples
+            # getting k next neighbors
+            neighbors.append(distances[i][0])
+        return neighbors
 
-# Exemplo de uso:
-X_train = np.array([[1, 2], [2, 3], [3, 4], [4, 5]])
-y_train = np.array([0, 0, 1, 1])
+    def predict(self, samples):
+        predictions=[]
+        for sample in samples:
+            neighbors = self.get_neighbord_distance(sample)
+            # counting neighbors frequence
+            neighbors_counter = {}
+            for n in neighbors:
+                if n in neighbors_counter.keys():
+                    neighbors_counter[n] += 1
+                else:
+                    neighbors_counter[n] = 0
+            # getting most frequence neighbor
+            prediction=max(zip(neighbors_counter.values(),
+                               neighbors_counter.keys()))[1]
+            
+            predictions.append(prediction)
+        return predictions
 
-X_test = np.array([[5, 6], [6, 7]])
-
-knn = KNN(k=2)
-knn.fit(X_train, y_train)
-predictions = knn.predict(X_test)
-
-print("Predictions:", predictions)
